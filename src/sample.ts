af5cc60e82288d9c9d86d49a190f9a2334847b2e
Richard Ibarra <ribarra@Richards-MacBook-Pro.local>
2019-12-05 16:30:06 -0300

    level 1. setup environment for testing

---
+++ src/sample.ts
@@ -0,0 +1,14 @@
+/*
+  Project. Tech Leader Test
+  Author. Richard Ibarra Ramírez - richard.ibarra@gmail.com
+ */
+
+
+// always return true
+export function returnTrue()
+{
+  if(true)
+    return true;
+
+  else return false;
+}
